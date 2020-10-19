e4ffd5ce42b0a54e276c33d1d51e7b3de271a696
TheMichaelJiles <sjiles1@my.westga.edu>
2020-10-19 18:09:51 -0400

    Added SearchForPatient function to PatientDAL

--- ClinicManagementSystem/DB/ModelDAL/PatientDAL.cs
+++ ClinicManagementSystem/DB/ModelDAL/PatientDAL.cs
@@ -3,15 +3,20 @@ using ClinicManagementSystem.Util;
 using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ClinicManagementSystem.DB.ModelDAL
 {
 	public class PatientDAL
 	{
 		private const string GetNurseQuery = "CALL InsertPatient(@fname, @lname, @DOB, @phone, @gender, @address1, @address2, @city, @state, @zip, @patientID)";
+		private const string SearchPatientQuery = "CALL SelectPatientsByNameDOB(@fname, @lname, @DOB)";
 		private const string GetMaxPatientID = "SELECT patientID FROM Patient ORDER BY patientID DESC LIMIT 1";
 
 		public static void InsertNewPatient(Patient patient)
@@ -52,6 +57,69 @@ namespace ClinicManagementSystem.DB.ModelDAL
 			}
 		}
 
+		public static IList<Patient> SearchForPatient(string fname, string lname, DateTime dob)
+		{
+			var connection = DbConnection.GetConnection();
+
+			using (connection)
+			{
+				connection.Open();
+
+				using (MySqlCommand cmd = new MySqlCommand(SearchPatientQuery, connection))
+				{
+					cmd.Parameters.AddWithValue("@fname", fname);
+					cmd.Parameters.AddWithValue("@lname", lname);
+					cmd.Parameters.AddWithValue("@DOB", dob);
+
+					return buildPatientList(cmd);
+				}
+			}
+		} 
+
+		private static IList<Patient> buildPatientList(MySqlCommand cmd)
+        {
+			IList<Patient> patients = new List<Patient>();
+
+			using (MySqlDataReader reader = cmd.ExecuteReader())
+			{
+				int patientIDOrdinal = reader.GetOrdinal("patientID");
+
+				int bioIDOrdinal = reader.GetOrdinal("ID");
+				int fnameOrdinal = reader.GetOrdinal("fname");
+				int lnameOrdinal = reader.GetOrdinal("lname");
+				int dobOrdinal = reader.GetOrdinal("DOB");
+				int phoneNumberOrdinal = reader.GetOrdinal("phone#");
+				int genderOrdinal = reader.GetOrdinal("gender");
+
+				int addressIDOrdinal = reader.GetOrdinal("addressID");
+				int address1Ordinal = reader.GetOrdinal("address1");
+				int address2Ordinal = reader.GetOrdinal("address2");
+				int cityOrdinal = reader.GetOrdinal("city");
+				int stateOrdinal = reader.GetOrdinal("state");
+				int zipOrdinal = reader.GetOrdinal("zip");
+
+				while (reader.Read())
+                {
+					Patient patient = new Patient();
+					patient.ID = DbDefault.GetString(reader, patientIDOrdinal);
+
+					patient.Bio.FirstName = DbDefault.GetString(reader, fnameOrdinal);
+					patient.Bio.LastName = DbDefault.GetString(reader, lnameOrdinal);
+					patient.Bio.DOB = DbDefault.GetDatetime(reader, dobOrdinal);
+					patient.Bio.PhoneNumber = DbDefault.GetString(reader, phoneNumberOrdinal);
+					patient.Bio.Gender = DbDefault.GetString(reader, genderOrdinal);
+
+					patient.Bio.Address.AddressID = DbDefault.GetInt(reader, addressIDOrdinal);
+					patient.Bio.Address.Address1 = DbDefault.GetString(reader, address1Ordinal);
+					patient.Bio.Address.Address2 = DbDefault.GetString(reader, address2Ordinal);
+					patient.Bio.Address.City = DbDefault.GetString(reader, cityOrdinal);
+					patient.Bio.Address.State = DbDefault.GetString(reader, stateOrdinal);
+					patient.Bio.Address.Zip = DbDefault.GetInt(reader, zipOrdinal);
+					patients.Add(patient);
+                }
+				return patients;
+			}
+        }
 		private static string buildPatientID(MySqlConnection connection)
 		{
 			using (MySqlCommand cmd = new MySqlCommand(GetMaxPatientID, connection))
--- ClinicManagementSystem/View/UserControls/SearchPatientControl.cs
+++ ClinicManagementSystem/View/UserControls/SearchPatientControl.cs
@@ -16,5 +16,10 @@ namespace ClinicManagementSystem.View.UserControls
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void searchPatientButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
