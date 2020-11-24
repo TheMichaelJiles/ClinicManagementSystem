b800ed75026e44d79bba8874e7b2052a269a9197
TheMichaelJiles <sjiles1@my.westga.edu>
2020-11-23 19:35:12 -0500

    Fixed datagrid, added to readme

--- ClinicManagementSystem/View/UserControls/AdminQueryPage.Designer.cs
+++ ClinicManagementSystem/View/UserControls/AdminQueryPage.Designer.cs
@@ -28,113 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
-			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-			this.title = new System.Windows.Forms.Label();
-			this.resultsDataGrid = new System.Windows.Forms.DataGridView();
-			this.executeButton = new System.Windows.Forms.Button();
-			this.queryTextBox = new System.Windows.Forms.RichTextBox();
-			((System.ComponentModel.ISupportInitialize)(this.resultsDataGrid)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// title
-			// 
-			this.title.AutoSize = true;
-			this.title.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.title.Location = new System.Drawing.Point(14, 12);
-			this.title.Name = "title";
-			this.title.Size = new System.Drawing.Size(270, 37);
-			this.title.TabIndex = 37;
-			this.title.Text = "SQL Query Terminal";
-			// 
-			// resultsDataGrid
-			// 
-			this.resultsDataGrid.AllowUserToAddRows = false;
-			dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.resultsDataGrid.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
-			this.resultsDataGrid.BackgroundColor = System.Drawing.Color.SlateGray;
-			this.resultsDataGrid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
-			dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-			dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
-			dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
-			dataGridViewCellStyle2.NullValue = null;
-			dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.SteelBlue;
-			dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-			dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-			this.resultsDataGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
-			this.resultsDataGrid.ColumnHeadersHeight = 25;
-			this.resultsDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-			dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-			dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
-			dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
-			dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-			dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-			dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-			this.resultsDataGrid.DefaultCellStyle = dataGridViewCellStyle3;
-			this.resultsDataGrid.Location = new System.Drawing.Point(21, 52);
-			this.resultsDataGrid.Name = "resultsDataGrid";
-			this.resultsDataGrid.ReadOnly = true;
-			dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-			dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
-			dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
-			dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.SteelBlue;
-			dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-			dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-			this.resultsDataGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
-			this.resultsDataGrid.RowHeadersVisible = false;
-			this.resultsDataGrid.RowHeadersWidth = 50;
-			dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.SteelBlue;
-			this.resultsDataGrid.RowsDefaultCellStyle = dataGridViewCellStyle5;
-			this.resultsDataGrid.RowTemplate.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-			this.resultsDataGrid.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.resultsDataGrid.RowTemplate.Height = 50;
-			this.resultsDataGrid.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
-			this.resultsDataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-			this.resultsDataGrid.Size = new System.Drawing.Size(735, 323);
-			this.resultsDataGrid.TabIndex = 38;
-			// 
-			// executeButton
-			// 
-			this.executeButton.BackColor = System.Drawing.Color.SteelBlue;
-			this.executeButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-			this.executeButton.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.executeButton.Location = new System.Drawing.Point(538, 510);
-			this.executeButton.Name = "executeButton";
-			this.executeButton.Size = new System.Drawing.Size(218, 36);
-			this.executeButton.TabIndex = 40;
-			this.executeButton.Text = "Execute Query";
-			this.executeButton.UseVisualStyleBackColor = false;
-			this.executeButton.Click += new System.EventHandler(this.executeButton_Click);
-			// 
-			// queryTextBox
-			// 
-			this.queryTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.queryTextBox.Location = new System.Drawing.Point(21, 381);
-			this.queryTextBox.Name = "queryTextBox";
-			this.queryTextBox.Size = new System.Drawing.Size(735, 123);
-			this.queryTextBox.TabIndex = 41;
-			this.queryTextBox.Text = "";
-			// 
-			// AdminQueryPage
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-			this.Controls.Add(this.queryTextBox);
-			this.Controls.Add(this.executeButton);
-			this.Controls.Add(this.title);
-			this.Controls.Add(this.resultsDataGrid);
-			this.Name = "AdminQueryPage";
-			this.Size = new System.Drawing.Size(909, 558);
-			((System.ComponentModel.ISupportInitialize)(this.resultsDataGrid)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.title = new System.Windows.Forms.Label();
+            this.resultsDataGrid = new System.Windows.Forms.DataGridView();
+            this.executeButton = new System.Windows.Forms.Button();
+            this.queryTextBox = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.resultsDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(14, 12);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(270, 37);
+            this.title.TabIndex = 37;
+            this.title.Text = "SQL Query Terminal";
+            // 
+            // resultsDataGrid
+            // 
+            this.resultsDataGrid.AllowUserToAddRows = false;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultsDataGrid.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.resultsDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.resultsDataGrid.BackgroundColor = System.Drawing.Color.SlateGray;
+            this.resultsDataGrid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.NullValue = null;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.SteelBlue;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.resultsDataGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.resultsDataGrid.ColumnHeadersHeight = 25;
+            this.resultsDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.resultsDataGrid.DefaultCellStyle = dataGridViewCellStyle3;
+            this.resultsDataGrid.Location = new System.Drawing.Point(21, 52);
+            this.resultsDataGrid.Name = "resultsDataGrid";
+            this.resultsDataGrid.ReadOnly = true;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.SteelBlue;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.resultsDataGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.resultsDataGrid.RowHeadersVisible = false;
+            this.resultsDataGrid.RowHeadersWidth = 50;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.SteelBlue;
+            this.resultsDataGrid.RowsDefaultCellStyle = dataGridViewCellStyle5;
+            this.resultsDataGrid.RowTemplate.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.resultsDataGrid.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultsDataGrid.RowTemplate.Height = 50;
+            this.resultsDataGrid.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.resultsDataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.resultsDataGrid.Size = new System.Drawing.Size(735, 323);
+            this.resultsDataGrid.TabIndex = 38;
+            // 
+            // executeButton
+            // 
+            this.executeButton.BackColor = System.Drawing.Color.SteelBlue;
+            this.executeButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.executeButton.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.executeButton.Location = new System.Drawing.Point(538, 510);
+            this.executeButton.Name = "executeButton";
+            this.executeButton.Size = new System.Drawing.Size(218, 36);
+            this.executeButton.TabIndex = 40;
+            this.executeButton.Text = "Execute Query";
+            this.executeButton.UseVisualStyleBackColor = false;
+            this.executeButton.Click += new System.EventHandler(this.executeButton_Click);
+            // 
+            // queryTextBox
+            // 
+            this.queryTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.queryTextBox.Location = new System.Drawing.Point(21, 381);
+            this.queryTextBox.Name = "queryTextBox";
+            this.queryTextBox.Size = new System.Drawing.Size(735, 123);
+            this.queryTextBox.TabIndex = 41;
+            this.queryTextBox.Text = "";
+            // 
+            // AdminQueryPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Controls.Add(this.queryTextBox);
+            this.Controls.Add(this.executeButton);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.resultsDataGrid);
+            this.Name = "AdminQueryPage";
+            this.Size = new System.Drawing.Size(909, 558);
+            ((System.ComponentModel.ISupportInitialize)(this.resultsDataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
